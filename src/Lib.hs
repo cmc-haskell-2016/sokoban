b030242a71db03c00d09bbd9c2033a49e4a53cee
Maksim <maxapple@yandex.ru>
2016-04-05 14:52:14 +0400

    Update Lib.hs
    4 main data structures moved to external module lib.hs

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,53 @@
 module Lib (
-  someFunc
+   Menu(..)
+  ,Game(..)
+  ,SokobanMap(..)
+  ,MapTextures(..)
 ) where
 
-someFunc :: String -> String
-someFunc s = "Hello, " ++ s ++ "!"
+import Graphics.Gloss.Data.Picture
+
+type Coord = (Int, Int)
+
+data Game = Game
+                {sokobanMaps :: [SokobanMap]
+                ,currMap     :: SokobanMap
+                ,backupMap   :: SokobanMap
+                ,stateBackup :: SokobanMap
+                ,currNumber  :: Int
+                ,menu        :: Menu
+                ,state       :: Int   -- 0 if currMap, 1 if Menu
+                ,scaleAll    :: Float
+                } deriving Show
+
+data Menu = Menu
+                {menuBackground   :: Picture
+                ,labelHeader      :: Picture
+                ,labelScaleInc    :: Picture
+                ,labelScaleDec    :: Picture
+                ,labelRestart     :: Picture
+                ,labelRestartGame :: Picture
+                ,labelPrevState   :: Picture
+                } deriving Show
+
+data SokobanMap = SokobanMap
+                          {size      :: Int
+                          ,walls     :: [Coord]
+                          ,targets   :: [Coord]
+                          ,boxes     :: [Coord]
+                          ,darkboxes :: [Coord]
+                          ,spaces    :: [Coord]
+                          ,player    :: Coord
+                          ,steps     :: Int
+                          ,textures  :: MapTextures
+                          } deriving Show
+
+data MapTextures = MapTextures
+                    {bgTexture      :: Picture
+                    ,wallTexture    :: Picture
+                    ,spaceTexture   :: Picture
+                    ,boxTexture     :: Picture
+                    ,darkboxTexture :: Picture
+                    ,targetTexture  :: Picture
+                    ,playerTexture  :: Picture
+                    } deriving Show
